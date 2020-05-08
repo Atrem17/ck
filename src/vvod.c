5cef97f7fe5cb287e392f99e3db1940a7f0bf7a7
ArtemMakaro <varvara.artem@yandex.ru>
2020-05-08 09:50:21 +0700

    A

--- src/vvod.c
+++ src/vvod.c
@@ -1,34 +1,35 @@
-#include "vvod.h"
 #include "authors.h"
 #include "menu.h"
 #include "pravila.h"
+#include "vvod.h"
 #define ESCAPE 27
 
-void vvod() {
-  setlocale(LC_ALL, "UTF-8");
-  char key;
-  do {
-    scanf("%c%*c", &key);
-    switch (key) {
-    case '0':
-      system("clear");
-      pravila();
-      break;
-    case '1':
-      system("clear");
-      print1();
-      break;
-    case '2':
-      system("clear");
-      authors();
-      break;
-    case '3':
-      system("clear");
-      printf("До новых встреч!");
-      return;
-      break;
-    default:
-      break;
-    }
-  } while (key != 0 && key != 1 && key != 2 && key != 3);
+void vvod()
+{
+    setlocale(LC_ALL, "UTF-8");
+    char key;
+    do {
+        scanf("%c%*c", &key);
+        switch (key) {
+        case '0':
+            system("clear");
+            pravila();
+            break;
+        case '1':
+            system("clear");
+            print1();
+            break;
+        case '2':
+            system("clear");
+            authors();
+            break;
+        case '3':
+            system("clear");
+            printf("До новых встреч!");
+            return;
+            break;
+        default:
+            break;
+        }
+    } while (key != 0 && key != 1 && key != 2 && key != 3);
 }
